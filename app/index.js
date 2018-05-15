0a918482ac9eb8edc45806dee8116ccf56545b1b
LordDurus <2650021+LordDurus@users.noreply.github.com>
2018-05-15 11:09:01 -0400

    Updated comments

--- app/index.js
+++ app/index.js
@@ -1,9 +1,24 @@
 /*
  * Entry point for the watch app
+ *!* ******************************************************************************
+ *!* Images *!*
  * Space Invader png came from https://www.game.es/videojuegos
  * Images from https://www.iconfinder.com
+ *    -- back.png
+ *    -- course.png
+ *    -- green-light.png
+ *    -- pick-players.png
+ *    -- previous-hole.png
+ *    -- user-green.png
  * Abacus.png (icon.png) from Aha-Soft
+ * Cthulhu from http://www.softicons.com/object-icons/richs-misc-icons-by-rich-d/cthulhu-icon
+ *    -- The licensor does not endorse this application in any way.
+ *    -- I have made no change to the Cthulhu image
+ *    -- Link to the license https://creativecommons.org/licenses/by-nc-sa/3.0/
+ *!* ******************************************************************************
+ *!* Data *!*
  * Got the course data for Flatrocks / Eagles Next from https://www.dgcoursereview.com/course.php?id=60
+ *!* ******************************************************************************
  */
 import document from 'document';
 import {
