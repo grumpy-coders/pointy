17454793c42aef321d5950bde4085d98b2d15738
LordDurus <2650021+LordDurus@users.noreply.github.com>
2018-06-08 11:27:57 -0400

    Fixed bug when showing the default course

--- app/screens/main.js
+++ app/screens/main.js
@@ -1,27 +1,27 @@
 import document from 'document';
 import {
-  screenGameInit
+	screenGameInit
 } from './game-golf.js';
 import {
-  screenSelectPlayersInit
+	screenSelectPlayersInit
 } from './select-player.js';
 import {
-  selectCourse
+	selectCourse
 } from './select-item.js'
 import {
-  selectGame
+	selectGame
 } from './select-item.js'
 import {
-  me
+	me
 } from "appbit";
 import * as st from './screen-tools.js'
 import * as constants from '../constants.js'
 import * as fs from 'fs';
 import {
-  GameState
+	GameState
 } from '../classes/GameState.js'
 import {
-  unbindEvents
+	unbindEvents
 } from './alert.js';
 
 /** @function screenMainInit
@@ -30,78 +30,79 @@ import {
  */
 export function screenMainInit(gameState) {
 
-  if (gameState == null) {
-    console.log('gameState is null');
-    let gameID = getDefaultID(constants.DEFAULT_GAME_FILE_PATH);
-    let courseID = getDefaultID(constants.DEFAULT_GAME_FILE_PATH);
+	if (gameState == null) {
+		console.log('gameState is null');
+		let gameID = getDefaultID(constants.DEFAULT_GAME_FILE_PATH);
+		let courseID = getDefaultID(constants.DEFAULT_GAME_FILE_PATH);
 
-    gameState = new GameState(gameID, courseID, [])
+		gameState = new GameState(gameID, courseID, [])
 
-  }
+	}
 
-  document.onkeypress = function (e) {
-    e.preventDefault();
-    switch (e.key) {
-      case "up":
-        console.log('main.up');
-        break;
-      case "down":
-        console.log('main.down');
-        break;
-      case "back":
-        console.log('main.back');
-        let srnYorN = document.getElementById('srnYorN');
-        srnYorN.getElementById('title').text = 'Exit?';
-        srnYorN.getElementById('btnYes').onclick = function () {
-          me.exit();
-        }
-        srnYorN.getElementById('btnNo').onclick = function () {
-          st.showScreen('srnMain');
-        }
-        st.showScreen(srnYorN.id);
-    }
-  }
+	document.onkeypress = function (e) {
+		e.preventDefault();
+		switch (e.key) {
+			case "up":
+				console.log('main.up');
+				break;
+			case "down":
+				console.log('main.down');
+				break;
+			case "back":
+				console.log('main.back');
+				let srnYorN = document.getElementById('srnYorN');
+				srnYorN.getElementById('title').text = 'Exit?';
+				srnYorN.getElementById('btnYes').onclick = function () {
+					me.exit();
+				}
+				srnYorN.getElementById('btnNo').onclick = function () {
+					st.showScreen('srnMain');
+				}
+				st.showScreen(srnYorN.id);
+		}
+	}
 
-  let scrMain = document.getElementById('srnMain');
-  let btnSelectGame = scrMain.getElementById('btnSelectGame')
-  if (gameState.game == null) {
-    btnSelectGame.text = 'Pick Game';
-  } else {
-    btnSelectGame.text = gameState.game.shortname.substring(0, 11).trim();
-  }
-  btnSelectGame.onclick = function () {
-    selectGame(gameState);
-  }
+	let scrMain = document.getElementById('srnMain');
+	let btnSelectGame = scrMain.getElementById('btnSelectGame')
+	if (gameState.game == null) {
+		btnSelectGame.text = 'Pick Game';
+	} else {
+		btnSelectGame.text = gameState.game.shortname.substring(0, 11).trim();
+	}
+	btnSelectGame.onclick = function () {
+		selectGame(gameState);
+	}
 
-  let btnSelectCourse = scrMain.getElementById('btnSelectCourse');
-  if (gameState.game == null) {
-    btnSelectCourse.text = 'Pick Course';
-  } else {
-    btnSelectCourse.text = gameState.course.shortname.substring(0, 11).trim();
-  }
-  btnSelectCourse.onclick = function () {
-    selectCourse(gameState);
-  }
+	let btnSelectCourse = scrMain.getElementById('btnSelectCourse');
+	if (gameState.course == null) {
+		btnSelectCourse.text = 'Pick Course';
+	} else {
+		btnSelectCourse.text = gameState.course.shortname.substring(0, 11).trim();
+	}
 
-  let btnPickPlayers = scrMain.getElementById('btnPickPlayers');
-  btnPickPlayers.text = 'Pick Players';
-  btnPickPlayers.onclick = function () {
-    screenSelectPlayersInit(gameState);
-  }
+	btnSelectCourse.onclick = function () {
+		selectCourse(gameState);
+	}
 
-  scrMain.getElementById('btnStart').onclick = function () {
-    // Unbind the click fixes the "Fitbit OS Simulator" from lossing connection.
-    document.onkeypress = null;
-    scrMain.getElementById('btnStart').onclick = null;
-    btnSelectGame.onclick = null;
-    btnSelectCourse.onclick = null;
-    btnPickPlayers.onclick = null;
-    screenGameInit(gameState);
-  }
-  scrMain.getElementsByClassName("main-button").forEach((element, index) => {
-    element.height = gameState.deviceInfo.squareButtonIconSize;
-  });
-  st.showScreen(scrMain.id);
+	let btnPickPlayers = scrMain.getElementById('btnPickPlayers');
+	btnPickPlayers.text = 'Pick Players';
+	btnPickPlayers.onclick = function () {
+		screenSelectPlayersInit(gameState);
+	}
+
+	scrMain.getElementById('btnStart').onclick = function () {
+		// Unbind the click fixes the "Fitbit OS Simulator" from lossing connection.
+		document.onkeypress = null;
+		scrMain.getElementById('btnStart').onclick = null;
+		btnSelectGame.onclick = null;
+		btnSelectCourse.onclick = null;
+		btnPickPlayers.onclick = null;
+		screenGameInit(gameState);
+	}
+	scrMain.getElementsByClassName("main-button").forEach((element, index) => {
+		element.height = gameState.deviceInfo.squareButtonIconSize;
+	});
+	st.showScreen(scrMain.id);
 }
 
 /** @function getDefaultID
@@ -109,11 +110,11 @@ export function screenMainInit(gameState) {
  * @param {string} defaultFilePath Path to default file.
  */
 function getDefaultID(defaultFilePath) {
-  let id;
-  try {
-    id = JSON.parse(fs.readFileSync(defaultFilePath, 'json')).selected;
-  } catch (error) {
-    id = 0;
-  }
-  return id;
+	let id;
+	try {
+		id = JSON.parse(fs.readFileSync(defaultFilePath, 'json')).selected;
+	} catch (error) {
+		id = 0;
+	}
+	return id;
 }
