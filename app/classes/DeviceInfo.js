b186029befc6e2bf9a0099dbebb01e74cb49aa7d
Durus <2650021+LordDurus@users.noreply.github.com>
2020-07-20 13:40:15 -0400

    Updated formating.
    VS Code finally fixed it's formatter.

--- app/classes/DeviceInfo.js
+++ app/classes/DeviceInfo.js
@@ -4,24 +4,23 @@ import { me as device } from 'device';
 * Has information about the device (watch)
 */
 export class DeviceInfo {
-  
   type = device.type;
   modelName = device.modelName;
   modelID = device.modelId;
   firmwareVersion = device.firmwareVersion;
   squareButtonIconSize = 0;
-    
+
   /**
    * Create a DeviceInfo
   */
-  constructor() {      
+  constructor() {
     if (device.modelName === "Versa") {
       this.screenHeight = 300;
       this.screenWidth = 300;
     } else {
       // Default to the ionic
       this.screenHeight = 250;
-      this.screenWidth = 348;         
+      this.screenWidth = 348;
     }
     this.squareButtonIconSize = this.screenHeight / 4;
   }
@@ -30,7 +29,7 @@ export class DeviceInfo {
    * Gets the last time the device was synced
    * @return {datetime} Returns void.
   */
-   lastSynced() { return device.lastSyncTime; }
+  lastSynced() { return device.lastSyncTime; }
 
   /**
    * Gets a string version of the object.
